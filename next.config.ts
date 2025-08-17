f6bac2f4883869f523e6617a5b285efc0b42e883
fajrin.rizky <fajrin.rizky@bukalapak.com>
2025-08-17 22:16:07 +0700

    add base path

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,11 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
-    remotePatterns: [new URL('https://raw.githubusercontent.com/PokeAPI/**')]
-  }
+    remotePatterns: [new URL('https://raw.githubusercontent.com/PokeAPI/**')],
+    unoptimized: true
+  },
+  basePath: '/pokemon', // ✅ your repo name
+  assetPrefix: '/pokemon/'
 }
 
 export default nextConfig
